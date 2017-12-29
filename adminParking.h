ec732fb74d8cc88109397b6db6cc163525cd1234
Shevraar <sky050577@gmail.com>
2017-12-29 15:03:33 +0100

    juste des modif mineures avant de copier en local pour moi

--- adminParking.h
+++ adminParking.h
@@ -3,7 +3,7 @@ int admin_already_exist(MYSQL *mysql,char *registration);
 int admin_already_parked(MYSQL *mysql, char *registration);
 void admin_add_car(MYSQL *mysql);
 void admin_delete_car(MYSQL *mysql);
-void client_add_car(MYSQL *mysql);
+
 
 
 char admin_menu(MYSQL *mysql)
@@ -132,28 +132,3 @@ void admin_delete_car(MYSQL *mysql)
     }
 }
 
-
-void client_add_car(MYSQL *mysql)
-{
-    char registration[15];
-    char request[150];
-    int subscribe =0;
-    int exist = 0;
-
-    printf("Enter your car registration : ");
-    fgets(registration,15,stdin);
-    if(strlen(registration)<14)
-        registration[strlen(registration)-1]='\0';
-
-    /**faire vérif plaque**/
-    /**faire vérif non existant dans la bdd**/
-    exist = admin_already_exist(mysql,registration);
-    if(!exist)
-    {
-        /**demander s'il veut un abonnement**/
-
-        /**ajouter le véhicule**/
-        sprintf(request,"INSERT INTO `voiture` (`id`, `plaque_immatriculation`, `abonnement`, `etat`) VALUES (NULL, '%s', '%d', '0')",registration,subscribe);
-        mysql_query(mysql,request);
-    }
-}
--- clientParking.h
+++ clientParking.h
@@ -1,1 +1,28 @@
+void client_add_car(MYSQL *mysql);
+
+
+void client_add_car(MYSQL *mysql)
+{
+    char registration[15];
+    char request[150];
+    int subscribe =0;
+    int exist = 0;
+
+    printf("Enter your car registration : ");
+    fgets(registration,15,stdin);
+    if(strlen(registration)<14)
+        registration[strlen(registration)-1]='\0';
+
+    /**faire vérif plaque**/
+    /**faire vérif non existant dans la bdd**/
+    exist = admin_already_exist(mysql,registration);
+    if(!exist)
+    {
+        /**demander s'il veut un abonnement**/
+
+        /**ajouter le véhicule**/
+        sprintf(request,"INSERT INTO `voiture` (`id`, `plaque_immatriculation`, `abonnement`, `etat`) VALUES (NULL, '%s', '%d', '0')",registration,subscribe);
+        mysql_query(mysql,request);
+    }
+}
 
